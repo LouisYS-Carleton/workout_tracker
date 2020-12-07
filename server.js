e0cff8944a44ec959776d3362bed852065aeb72c
Louis Yacksmith <louisyscarleton@gmail.com>
2020-12-06 20:25:16 -0500

    add rough basic code

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useCreateIndex: true,
 });
 
-app.use(require("./routes/api.js"));
+app.use(require("./routes/routes_api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
