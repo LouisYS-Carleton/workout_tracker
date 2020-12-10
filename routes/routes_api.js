4a4a4e57fa95a35e06334fc5f28c5d4af8375ba8
Louis Yacksmith <louisyscarleton@gmail.com>
2020-12-09 22:43:53 -0500

    finished all routes, troubleshooting NaN prompt in app.

--- routes/routes_api.js
+++ routes/routes_api.js
@@ -1,21 +1,41 @@
 const router = require("express").Router();
-const db = require("../models");
+const Workout = require("../models/model_workout.js");
 
-router.get("/api/workouts", async (req, res) => {
-  try {
-    const workouts = await db.Workout.find({});
-    res.status(200).json(workouts);
-  } catch (err) {
-    res.status(500).json({ error: err });
-  }
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then((dataWorkout) => res.json(dataWorkout))
+    .catch((err) => res.json(err));
 });
 
-router.post("/api/workouts", async (req, res) => {});
+router.post("/api/workouts", ({ body }, res) => {
+  console.log("Post method successful.", { body });
+  Workout.create(body)
+    .then((dataWorkout) => {
+      res.json(dataWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
-router.put("/api/workouts/:id", async (req, res) => {});
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(
+    req.params.id,
+    { $push: { exercises: req.body } },
+    { new: true }
+  )
+    .then((workout) => res.json(workout))
+    .catch((err) => res.json(err));
+});
 
-router.get("/api/workouts/range", async (req, res) => {});
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .then((dataWorkout) => {
+      res.json(dataWorkout);
+    })
+    .catch((err) => res.json(err));
+});
 
-router.delete("/api/workout", (req, res) => {});
+router.delete("/api/workouts", (req, res) => {});
 
 module.exports = router;
